1e4726583a7961a3deeccd44e299a269f49b84a7
Joseph Lander <josephclander@gmail.com>
2025-04-08 12:58:04 +0100

    update footer social links

--- src/components/footer/footer-social-links.tsx
+++ src/components/footer/footer-social-links.tsx
@@ -5,29 +5,19 @@ import { SocialLink } from '@/interfaces/social-link'
 
 export const socialLinks: SocialLink[] = [
   {
-    name: 'Instagram',
-    link: '#',
-    icon: '/images/icons/instagram.svg',
-  },
-  {
     name: 'YouTube',
-    link: '#',
+    link: 'https://www.youtube.com/@LargeBytes',
     icon: '/images/icons/youtube.svg',
   },
   {
-    name: 'Twitter',
-    link: '#',
-    icon: '/images/icons/twitter.svg',
-  },
-  {
-    name: 'Dribbble',
-    link: 'https://dribbble.com/shots/18114471-Coursespace-Online-Course-Landing-Page',
-    icon: '/images/icons/dribbble.svg',
-  },
-  {
     name: 'Github',
-    link: 'https://github.com/hiriski/coursespace-landing-page',
+    link: 'https://github.com/josephclander',
     icon: '/images/icons/github.svg',
+  },
+  {
+    name: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/josephclander/',
+    icon: '/images/icons/linkedin.svg',
   },
 ]
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ const DynamicHomeHero = dynamic(() => import('../components/home/hero'))
 const DynamicHomeFeature = dynamic(() => import('../components/home/feature'))
 const DynamicHomePopularCourse = dynamic(() => import('../components/home/popular-courses'))
 const DynamicHomeTestimonial = dynamic(() => import('../components/home/testimonial'))
-const DynamicHomeOurMentors = dynamic(() => import('../components/home/mentors'))
+// const DynamicHomeOurMentors = dynamic(() => import('../components/home/mentors'))
 // const DynamicHomeNewsLetter = dynamic(() => import('../components/home/newsletter'))
 
 const Home: NextPageWithLayout = () => {
@@ -18,7 +18,7 @@ const Home: NextPageWithLayout = () => {
       <DynamicHomeFeature />
       <DynamicHomePopularCourse />
       <DynamicHomeTestimonial />
-      <DynamicHomeOurMentors />
+      {/* <DynamicHomeOurMentors /> */}
       {/* <DynamicHomeNewsLetter /> */}
     </>
   )
